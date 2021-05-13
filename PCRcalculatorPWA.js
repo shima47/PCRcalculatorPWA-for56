7dd14445d02adac9a9188f7dc822d2b7a1c9382b
shima47 <82750432+shima47@users.noreply.github.com>
2021-05-14 00:44:35 +0900

    Update PCRcalculatorPWA.js

--- PCRcalculatorPWA.js
+++ PCRcalculatorPWA.js
@@ -2,6 +2,8 @@
 const $button = document.getElementById("caluculate");
 //フォームオブジェクトを取得
 const $form = document.getElementById("form");
+//name="oneVol"オブジェクト配列を取得
+const $oneVol = document.getElementsByName("oneVol");
 
 //ボタンを押すと実行
 $button.addEventListener("click",() => {
@@ -13,15 +15,29 @@ $form.addEventListener("keyup",() => {
 	caluculate();
 });
 
+//容量変更すると実行
+$oneVol.forEach((oneVolForm) => {
+	oneVolForm.addEventListener("keyup",() => {
+		caluculate();
+	});
+});
+
 //計算系
 const caluculate = () => {
 	//フォーム内の値を取得
 	const Num = $form.value;
-	
-	const oneVol = [7,1,1,0.075,0.8,9.075,9.875];
-	let resultVol = [];
+
+	//oneVolを配列に格納
+	let oneVol = [];
 	let oneVolIndex = 0;
+	while(oneVolIndex < $oneVol.length){
+		oneVol.push($oneVol[oneVolIndex].value);
+		oneVolIndex++;
+	}
+
 	//計算と格納
+	let resultVol = [];
+	oneVolIndex = 0;
 	while(oneVolIndex < oneVol.length){
 		let a = oneVol[oneVolIndex] * Num;
 		let b = a * 1000;
@@ -35,7 +51,7 @@ const caluculate = () => {
 	const $multiple = document.getElementById("multiple");
 	//最上段右の項目を書き換え
 	$multiple.textContent = "x" + Num;
-	
+
 	//name="result"オブジェクト配列を取得
 	const $result = document.getElementsByName("result");
 	//右列の項目を書き換え
